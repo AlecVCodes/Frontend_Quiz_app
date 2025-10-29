f672151ebecb436a59f6e87561071e699249ad63
AlecVCodes <alectvine@gmail.com>
2025-10-29 19:36:40 +1000

    initial commit
    basic setup

--- script.js
+++ script.js
@@ -3,7 +3,8 @@
 //
 const ThemeToggleCircle = document.querySelector(".toggle-circle");
 const ThemeToggleBtn = document.querySelector(".toggle-theme-btn");
-
+const sunIconFill = document.querySelector(".sun-icon > path");
+const moonIconFill = document.querySelector(".moon-icon > path");
 //
 // STATE MANAGEMENT
 //
@@ -28,18 +29,47 @@ ThemeToggleBtn.addEventListener("click", () => {
 });
 
 //
-// FUNCTIONS
+//FETCH QUIZ DATA
 //
+
+let allQuizData;
+
+fetch("./data.json")
+  .then((response) => {
+    if (!response.ok) return console.log("Error");
+
+    return response.json();
+  })
+  .then((data) => {
+    let allQuizData = data;
+
+    console.log(allQuizData, "all quiz data");
+  });
+
+//
+// <FUNCTIONS></FUNCTIONS>
+//
+
 function enableDarkMode() {
   console.log("enable dark mode");
+  // add dark mode classes
   document.body.classList.add("dark-mode");
   ThemeToggleCircle.classList.add("dark-mode");
+  // change fill of icons
+  moonIconFill.style.fill = "#fff";
+  sunIconFill.style.fill = "#fff";
+  //set dark mode value to enabled in local storage
   localStorage.setItem("darkMode", "enabled");
 }
 
 function disableDarkMode() {
-  console.log("disable dark mode");
+  //remove dark mode classes
+
   document.body.classList.remove("dark-mode");
   ThemeToggleCircle.classList.remove("dark-mode");
+  //change fill of icons
+
+  moonIconFill.style.fill = "#626C7F";
+  sunIconFill.style.fill = "#626C7F";
   localStorage.setItem("darkMode", null);
 }
