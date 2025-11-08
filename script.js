4e4b61b8230c5f13ea06524a42d396d0702cfed3
AlecVCodes <alectvine@gmail.com>
2025-11-08 15:48:25 +1000

    Functionality for UI
    basic UI functionality that shows the question and answers when user selects quiz and clicks submit button to change question

--- script.js
+++ script.js
@@ -3,52 +3,56 @@
 //
 
 /* theme toggle */
-const ThemeToggleCircle = document.querySelector(".toggle-circle");
-const ThemeToggleBtn = document.querySelector(".toggle-theme-btn");
+const themeToggleCircle = document.querySelector(".toggle-circle");
+const themeToggleBtn = document.querySelector(".toggle-theme-btn");
 const sunIconFill = document.querySelector(".sun-icon > path");
 const moonIconFill = document.querySelector(".moon-icon > path");
 
 /* main menu screen */
-
 const mainMenuScreen = document.querySelector("#main-menu");
 
 /* main menu quiz options */
-const quizOptions = document.querySelectorAll(".choose-quiz-option");
+const quizOptionButtons = document.querySelectorAll(".choose-quiz-option");
 
 /* quiz UI screen */
+const quizUIScreen = document.querySelector("#quiz-ui");
 
-const quizScreen = document.querySelector("#quiz-ui");
+/* quiz options ui */
+const quizQuestion = document.querySelector(".current-quiz-question");
 
-//next question button
+const quizAnswers = document.querySelectorAll(".quiz-list > li");
 
-const nextQuestionBtn = document.querySelector("#next-question-btn");
+const quizAnswerstitle = document.querySelectorAll(".quiz-answer-title");
+
+/* next question button */
+const nextQuestionButton = document.querySelector("#next-question-btn");
 
 //
 // STATE MANAGEMENT
 //
 let darkMode = localStorage.getItem("darkMode");
 
-let allQuizData;
+let quizLibrary; // all quizzes from JSON
+let selectedQuiz; // currently chosen quiz
+let isQuizLoaded = false;
 
-let currentQuiz;
-
-let doesQuizDataExist = false;
+let userInputtedAnswer;
 
 let quizScore = 0;
+let activeQuestionIndex = 0;
 
-let currentQuestionIndex = 0;
-
-let currentQuizData = {
-  title: "Math",
+let activeQuizState = {
+  title: "",
   questions: [
     {
-      question: "What is 1 + 1",
-      options: ["2", "3", "4", "5"],
+      question: "",
+      options: ["", "", "", ""],
     },
   ],
+  answer: "",
 };
 
-console.log(currentQuizData);
+console.log(activeQuizState);
 
 // Check saved theme on load
 if (darkMode === "enabled") {
@@ -58,7 +62,7 @@ if (darkMode === "enabled") {
 //
 // EVENT LISTENERS
 //
-ThemeToggleBtn.addEventListener("click", () => {
+themeToggleBtn.addEventListener("click", () => {
   darkMode = localStorage.getItem("darkMode");
 
   if (darkMode !== "enabled") {
@@ -68,14 +72,34 @@ ThemeToggleBtn.addEventListener("click", () => {
   }
 });
 
-quizOptions.forEach((quizOption, quizDataindex) => {
-  quizOption.addEventListener("click", () => {
-    fetchQuizData(quizDataindex);
+// main menu event listeners
+quizOptionButtons.forEach((quizOptionButton, quizIndex) => {
+  quizOptionButton.addEventListener("click", () => {
+    fetchQuizData(quizIndex);
+  });
+});
+
+// current quiz event listeners
+
+quizAnswers.forEach((answer, index) => {
+  answer.addEventListener("click", () => {
+    userInputtedAnswer = answer.textContent;
+
+    //add active class
+    showSelectedAnswer(answer);
+
+    return userInputtedAnswer;
   });
 });
 
 // update set of questions when user clicks on next question button
-nextQuestionBtn.addEventListener("click", changeQuestion);
+nextQuestionButton.addEventListener("click", () => {
+  // check answer of user
+  checkAnswer();
+
+  // after checking answer then change the ui
+  changeQuestionLogic();
+});
 
 //
 // FUNCTIONS
@@ -85,86 +109,117 @@ function enableDarkMode() {
   console.log("enable dark mode");
   // add dark mode classes
   document.body.classList.add("dark-mode");
-  ThemeToggleCircle.classList.add("dark-mode");
+  themeToggleCircle.classList.add("dark-mode");
   // change fill of icons
   moonIconFill.style.fill = "#fff";
   sunIconFill.style.fill = "#fff";
-  //set dark mode value to enabled in local storage
+  // set dark mode value to enabled in local storage
   localStorage.setItem("darkMode", "enabled");
 }
 
 function disableDarkMode() {
-  //remove dark mode classes
-
+  // remove dark mode classes
   document.body.classList.remove("dark-mode");
-  ThemeToggleCircle.classList.remove("dark-mode");
-  //change fill of icons
-
+  themeToggleCircle.classList.remove("dark-mode");
+  // change fill of icons
   moonIconFill.style.fill = "#626C7F";
   sunIconFill.style.fill = "#626C7F";
   localStorage.setItem("darkMode", null);
 }
 
 // Get Data for selected quiz
-
 function fetchQuizData(index) {
-  //FETCH QUIZ DATA
-  //
-
+  // FETCH QUIZ DATA
   fetch("./data.json")
     .then((response) => {
       if (!response.ok) return console.log("Error");
 
       return response.json();
     })
-
     .then((data) => {
-      //get data for all quizzes
-      allQuizData = data.quizzes;
+      // get data for all quizzes
+      quizLibrary = data.quizzes;
 
-      //get data for current quiz
-      currentQuiz = allQuizData[index];
+      // get data for current quiz
+      selectedQuiz = quizLibrary[index];
 
-      doesQuizDataExist = true;
-      return currentQuiz;
+      isQuizLoaded = true;
+      return selectedQuiz;
     });
 
   showQuizUI();
 }
 
-// show the full quiz ui
+// show the full quiz UI
 function showQuizUI() {
-  if (!doesQuizDataExist) return;
+  if (!isQuizLoaded) return;
 
-  //hide main menu
+  // hide main menu
   mainMenuScreen.classList.remove("active");
 
-  //show quiz ui
-  quizScreen.classList.add("active");
+  // show quiz UI
+  quizUIScreen.classList.add("active");
 
   // change the data
-  currentQuizData.title = currentQuiz.title;
+  activeQuizState.title = selectedQuiz.title;
 
-  // all current quiz questions
-  // quizData.questions = currentQuiz.questions;
-
-  console.log(currentQuizData, "current quiz data");
-  //show first set of questions
-  changeQuestion();
+  // show first set of questions
+  changeQuestionLogic();
 }
 
-function changeQuestion() {
-  if (currentQuestionIndex < currentQuiz.questions.length) {
-    let currentQuestionData = currentQuiz.questions[currentQuestionIndex];
+function changeQuestionLogic() {
+  if (activeQuestionIndex < selectedQuiz.questions.length) {
+    let activeQuestionData = selectedQuiz.questions[activeQuestionIndex];
 
-    let currentQuestionOptions = currentQuestionData.options;
+    let activeQuestionOptions = activeQuestionData.options;
 
-    currentQuestionOptions.forEach((option) => {
-      // currentQuizData.questions.options.push(option);
+    // change the question
+    activeQuizState.questions[0].question = activeQuestionData.question;
+
+    // change user options
+    activeQuestionOptions.forEach((option, index) => {
+      // add values to activeQuizState object
+      activeQuizState.questions[0].options[index] = option;
     });
 
-    console.log(currentQuestionData, "options");
-    //move to next question
-    currentQuestionIndex++;
+    // change the answer
+    activeQuizState.answer = activeQuestionData.answer;
+
+    // move to next question
+    activeQuestionIndex++;
+
+    //
+
+    console.log(activeQuizState, "active quiz state");
+
+    changeQuestionUI();
+  }
+}
+
+function changeQuestionUI() {
+  quizQuestion.textContent = activeQuizState.questions[0].question;
+
+  quizAnswerstitle.forEach((answer, index) => {
+    // loop through each question html element and show the corresponding question based on the index
+
+    answer.textContent = activeQuizState.questions[0].options[index];
+  });
+}
+
+function showSelectedAnswer(answer) {
+  quizAnswers.forEach((answer) => {
+    console.log(answer, "answer ui");
+    answer.classList.remove("active");
+  });
+  answer.classList.add("active");
+}
+
+function checkAnswer() {
+  console.log("check answer function ran");
+  // check if answer is incorrect
+  if (userInputtedAnswer !== activeQuizState.answer) {
+    return;
+  } else {
+    console.log("correct answer! well done");
   }
 }
